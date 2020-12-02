3f9bc61c5d790a94fad2be08eb2c28201b6f7569
eduardoLemes <eduardo_lsf@outlook.com>
2020-12-01 22:17:41 -0300

    commit

--- jogo.js
+++ jogo.js
@@ -18,7 +18,13 @@ let largura  = 0;
 adjustScreen();
 
 //Criando posição dos gatinhos aleatórios. 
-function posicaorandom(){
+function posicaoRandom(){
+
+//remover gato anterior, se tiver gato ne.  não sei pq isso daqui foi muito hard.
+if(document.getElementById('cat')){
+document.getElementById('cat').remove();
+}
+
 // math.floor para tirar deixar valor fechado e random com math.random
 let posicaoX = Math.floor(Math.random() * largura) - 90;  // -90 para a posição aleatória seja no máximo 90 px menos que o limite
 let posicaoY = Math.floor(Math.random() * altura) - 90; // -90 para a posição aleatória seja no máximo 90 px menos que o limite
@@ -28,14 +34,47 @@ posicaoX = posicaoX < 0 ? 0 : posicaoX;
 posicaoY = posicaoY < 0 ? 0 : posicaoY;
 console.log(posicaoX, posicaoY)
 
-//Criar elemento hhtml usando DOM.
+//Criar elemento html usando DOM.
 let cat = document.createElement('img');
 cat.src = 'images/cat2.png'; // pegando a imagem do gatinho
-cat.className = 'cat'; // pegando a class cat
+cat.className = tamanhoRandom() +' '+ ladoAleatorio(); // pegando a class cat e criando random, tamanho e lado
 cat.style.left = posicaoX + 'px'; // concatenando o style com posição
 cat.style.top = posicaoY + 'px';// concatenando o style com posição
 cat.style.position = 'absolute';// transformando em absoluto
+cat.id = 'cat'
 
 //adicionando um filho ao body
 document.body.appendChild(cat);
+
+
 }
+
+//Função para criar tamanho random dos gatinhos
+function tamanhoRandom(){
+  let classe = Math.floor(Math.random() * 3); // Resultado sera algo entre 0 e muito próximo de 3
+  // console.log(classe);
+  switch(classe){ // pegando nossa variavel classe para  mudar os tamanhos.
+    case 0:
+      return 'cat';
+    case 1:
+      return 'cat2';
+    case 2:
+      return 'cat3';
+    case 3:
+      return 'cat4';
+  }
+
+}
+// criando lado estabelecido por propriedades de estilo, transformando a escala da imagem.
+function ladoAleatorio(){
+  let classe = Math.floor(Math.random() * 2); // Resultado sera algo entre 0 e muito próximo de 2
+  // console.log(classe);
+  switch(classe){ // pegando nossa variavel classe para  mudar as dimensões 2D.
+    case 0:
+      return 'ladoA';
+    case 1:
+      return 'ladoB';
+   
+  }
+}
+
